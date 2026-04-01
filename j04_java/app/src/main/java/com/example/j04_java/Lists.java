5b3c698bb9cba01f84491b2d2f3411f5be143a23
mahmutozsoy <mahmutozsoy2604@gmail.com>
2026-04-01 21:54:11 +0300

    listeler

---
+++ j04_java/app/src/main/java/com/example/j04_java/Lists.java
@@ -0,0 +1,13 @@
+package com.example.j04_java;
+import java.util.ArrayList;
+
+public class Lists {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<String>();
+        cars.add("Volvo");
+        cars.add("BMW");
+        cars.add("Ford");
+        cars.add("Mazda");
+        System.out.println(cars);
+    }
+}
