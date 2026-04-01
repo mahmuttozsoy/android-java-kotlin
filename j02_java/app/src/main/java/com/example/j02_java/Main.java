f7dd161430f3082815200cc43603f37bdf25571c
mahmutozsoy <mahmutozsoy2604@gmail.com>
2026-04-01 21:30:13 +0300

    degiskenler kullanımı

---
+++ j02_java/app/src/main/java/com/example/j02_java/Main.java
@@ -0,0 +1,18 @@
+package com.example.j02_java;
+
+public class Main {
+    public static void main(String[] args) {
+        //variables
+        int sayi1=12;
+        int sayi2=20;
+        int toplam=sayi1+sayi2;
+        System.out.println(toplam);
+        int fark=sayi1-sayi2;
+        float bolum=sayi1/sayi2;
+        String ad="ahmet";
+        System.out.println(ad);
+        double bolum2=sayi2/sayi1;
+        System.out.println(fark);
+        System.out.println(sayi1*15);
+    }
+}
