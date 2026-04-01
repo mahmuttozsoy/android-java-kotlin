ac2402c06c8dde33d4137e09efcee125d60ee5fc
mahmutozsoy <mahmutozsoy2604@gmail.com>
2026-04-01 21:46:58 +0300

    arrays

---
+++ j03_java/app/src/main/java/com/example/j03_java/Arrays.java
@@ -0,0 +1,16 @@
+package com.example.j03_java;
+
+public class Arrays {
+    public static void main(String[] args) {
+      String[] myStringArray=new String[3];
+      myStringArray[0]="Hello";
+      myStringArray[1]="World";
+      myStringArray[2]="!";
+      System.out.println(myStringArray[0]);
+
+      int[] myNumberArray={1,2,3,4,5};
+        System.out.println(myNumberArray[2]);
+
+
+    }
+}
